1cc9b5b3b80264fafd19d565214c03e9affe779c
Sagarshindeb8 <sagarshindeb8@gmail.com>
2022-09-13 22:35:32 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+public username : string = '';
+public password : string = '';
+public isLoginSuccessful = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    if(this.username === 'admin' && this.password === 'admin') {
+      this.isLoginSuccessful = true ;
+    }
+    else {
+      this.isLoginSuccessful = false;
+    }
+  }
+
+}
